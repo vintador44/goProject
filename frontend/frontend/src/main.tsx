7bc2a98d4c4dc7b7d2b931d6b93cdec39b550d0b
vintador44 <154009206+vintador44@users.noreply.github.com>
2026-04-13 08:57:30 +0300

    AppRouter

--- frontend/frontend/src/main.tsx
+++ frontend/frontend/src/main.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-
+import React, { useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, useNavigate, Routes } from 'react-router-dom';
 import App from './App'
+import InformationPage from "../pages/InformationPage"
 
 const container = document.getElementById('root')
-
 const root = createRoot(container!)
 
+
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+    <Router>
+        <Routes>
+           
+            <Route path="/" element={<InformationPage />} />
+        </Routes>
+    </Router>
 )
